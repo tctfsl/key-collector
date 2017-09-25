f6b05ee2a08e7ab159f36d15f0857c99660b3507
MiguelOrino <31387037+MiguelOrino@users.noreply.github.com>
2017-09-25 22:54:30 +0800

    Add files via upload

--- GameBoardModel.java
+++ GameBoardModel.java
@@ -1,21 +1,29 @@
 public class GameBoardModel {
+	/*
+	@author Megat;
+	*/
   private Piece[] thePlayers;
   private int turn;
   
   public GameBoardModel() {
+	//Array to store players
     thePlayers = new Piece[4];
     
+	//Initializing the players from the PieceFactory
     for(int i = 0; i < thePlayers.length; i++) {
       thePlayers[i] = PieceFactory.getPiece(("Player " + String.valueOf(i + 1)));
     }
     
+	//Set the turn to 1
     turn = 1;
   }
   
+  //Method to get which player's turn it is to move
   public int getTurn() {
     return turn;
   }
   
+  //Method to get the array of players
   public Piece[] getThePlayers() {
     return thePlayers;
   }
--- GameBoardView.java
+++ GameBoardView.java
@@ -1,12 +1,21 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
-import java.util.Vector;
+import javax.swing.ImageIcon;			//To implement the use of ImageIcons
+import javax.swing.JPanel;				//To implement the use of the JPanel
+import java.awt.Color;					//To change the color of the background of the button
+import java.awt.GridLayout;				//To set the layout of the GameBoardView panel
+import java.awt.event.ActionListener;	//To implement the use of ActionListeners on the buttons
+import java.util.Vector;				//To implement the use of Vectors to store the player's possible moves
+
+/*
+	@author Megat, Miguel, Faisal;
+*/
 
 public class GameBoardView extends JPanel {
+  //The buttons for the game
   private Cell[][] bttn;
+  //Array of piece to store the players
   private Piece[] thePlayers = new Piece[4];
   
+  //Instantiate the keys from the PieceFactory
   private Piece monkey = PieceFactory.getPiece("monkey");
   private Piece donkey = PieceFactory.getPiece("donkey");
   private Piece keydisk = PieceFactory.getPiece("keydisk");
@@ -14,11 +23,13 @@ public class GameBoardView extends JPanel {
   private Piece keynote = PieceFactory.getPiece("keynote");
   private Piece chest = PieceFactory.getPiece("chest");
   
+  //GameBoardView constructor
   public GameBoardView() {
     setLayout(new GridLayout(9, 9));
   
     bttn = new Cell[9][9];
     
+	//Instantiate the players from the PieceFactory
     for(int i = 0; i < thePlayers.length; i++) {
       thePlayers[i] = PieceFactory.getPiece(("Player " + String.valueOf(i + 1)));
     }
@@ -36,6 +47,7 @@ public class GameBoardView extends JPanel {
       bttn[thePlayers[i].getX()][thePlayers[i].getY()].setPiece(thePlayers[i]);
     }
     
+	//Adding the chest and keys to the buttons
     bttn[chest.getX()][chest.getY()].setPiece(chest);
     bttn[monkey.getX()][monkey.getY()].setPiece(monkey);
     bttn[donkey.getX()][donkey.getY()].setPiece(donkey);
@@ -43,6 +55,7 @@ public class GameBoardView extends JPanel {
     bttn[pinkey.getX()][pinkey.getY()].setPiece(pinkey);
     bttn[keynote.getX()][keynote.getY()].setPiece(keynote);
     
+	//Setting the background of all buttons to default, and disables them
     for(int i = 0; i < 9; i++) {
       for(int j = 0; j < 9; j++) {
         bttn[i][j].setBackground(null);
@@ -52,11 +65,15 @@ public class GameBoardView extends JPanel {
       }
     }
     
+	//Instantiates a vector of vectors to store the player's possible moves
     Vector<Vector<Integer>> possibleMoves = new Vector<Vector<Integer>>();
+	
+	//Calls the calculateMoves() method from PlayerPiece to get the possibleMoves
     possibleMoves = ((PlayerPiece) thePlayers[0]).calculateMoves(bttn);
     Vector<Integer> rowList = possibleMoves.get(0);
     Vector<Integer> columnList = possibleMoves.get(1);
     
+	//For all the possible moves, the color of the button is set to red and enabled for the player to click
     for(int m = 0; m < rowList.size(); m++) {
       bttn[rowList.get(m)][columnList.get(m)].setBackground(Color.red);
       bttn[rowList.get(m)][columnList.get(m)].setEnabled(true);
@@ -64,10 +81,12 @@ public class GameBoardView extends JPanel {
     }
   }
   
+  //Method to get the array of buttons
   public Cell[][] getBttn() {
     return bttn;
   }
   
+  //Adds an action listener to each of the buttons
   public void addCellListener(ActionListener e) {
     for(int i = 0; i < 9; i++) {
       for(int j = 0; j < 9; j++) {
